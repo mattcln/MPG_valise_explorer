f3319820c078a2f43deeb8aa7dec29abb48eb38b
Matthieu COLIN <matthieu.cln@gmail.com>
2024-02-01 23:29:30 +0100

    Selenium working - local and docker

--- launch_selenium.py
+++ launch_selenium.py
@@ -1,12 +1,27 @@
-import requests
-from requests.auth import HTTPBasicAuth
-from selenium import webdriver
-
-# from bs4 import BeautifulSoup
-# import re
-from scrap.league import League
-from scrap.game import Game
 from config.config_reader import get_config
+import time
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+docker = False
+
+chrome_options = Options()
+
+if docker:
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()), options=chrome_options
+)
 
 config = get_config()
 
@@ -17,5 +32,9 @@ password = config["credentials"]["password"]
 # URL of the MPG login page
 url = "https://mpg.football/auth/login"
 
-driver = webdriver.Edge()
 driver.get(url)
+
+soup = BeautifulSoup(driver.page_source, features="lxml")
+
+print(soup)
+time.sleep(10)
