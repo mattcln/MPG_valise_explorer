28348ef09f2cdeb0198b6acdd7d490d09c2ee9de
Matthieu COLIN <matthieu.cln@gmail.com>
2024-02-03 11:59:56 +0100

    Selenium logging through class (not working in docker)

--- utils/driver.py
+++ utils/driver.py
@@ -4,11 +4,14 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 
 
 class Driver:
     chrome_options = Options()
+    url = "https://mpg.football/auth/login"
 
     def __init__(self, docker=True) -> None:
         if docker:
@@ -35,6 +38,26 @@ class Driver:
                 WebDriverWait(self.driver, 10).until(
                     EC.element_to_be_clickable((By.XPATH, button_XPATH))
                 ).click()
+                print("BINGO COOKIES")
                 break
             except:
                 print("Element not found")
+
+    def logging(self, user, password):
+        self.driver.get(self.url)
+        self.driver.fullscreen_window()
+
+        self.accept_cookies()
+
+        self.driver.implicitly_wait(10)
+
+        self.driver.find_element(By.ID, "login").send_keys(user)
+        self.driver.find_element(By.ID, "password").send_keys(password)
+
+        buttons = self.driver.find_elements(By.TAG_NAME, "button")
+        for button in buttons:
+            if button.text == "Je me connecte":
+                print("FOUND IT")
+                button.click()
+
+        time.sleep(100)
